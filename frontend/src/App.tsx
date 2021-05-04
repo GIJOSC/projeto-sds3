a0c9dc1e8dd8489f5addfd37c414e54f80204eb5
Gesiel Neves <neves.gesiel@gmail.com>
2021-05-03 23:16:15 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import Datatable from "components/DataTable"
+import Footer from "components/Footer"
+import NavBar from "components/NavBar"
+
+
 function App() {
   return (
+    <>
+     <NavBar/>
     <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-      
-  
+      <h1 className="text-primary"> Olá Mundo!</h1>
+      <Datatable/>
     </div>
+    <Footer/>
+    
+    </>
   );
 }
 
